4c62aab19589a017d253e04cffe7d1fc419f165e
Mihalytch <Mihalytch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-05 00:27:54 +0000

    new files for v.1.2
    git-svn-id: https://plugins.svn.wordpress.org/sbmodal/tags/1.2@1105556 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ core/options.php
@@ -0,0 +1,29 @@
+<?php
+defined('ABSPATH') or die("No script kiddies please!");
+
+class SBModalOptions {
+	private static $_defaults = null;
+
+	public static function defaults() {
+		if ( is_null( self::$_defaults ) ) {
+			self::$_defaults = array(
+				'sbmodal_bootstrapjs' => 1,
+				'sbmodal_bootstrapcss' => 'full',
+			);
+		}
+
+		return self::$_defaults;
+	}
+
+	public static function get( $option_name ) {
+		$defaults = self::defaults();
+
+		if ( !isset( $defaults[$option_name] ) ) {
+			throw new Exception("SBModalOptions: Undefined SBModal Option.");
+		}
+
+		$option_value = get_option( $option_name, $defaults[$option_name] );
+
+		return $option_value;
+	}
+}
